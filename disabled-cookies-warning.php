270ca1305461c5991433cd028ed9194d68c47121
ijerkov <ivojerkovic@gmail.com>
2023-05-04 15:07:42 +0200

    Update disabled-cookies-warning.php
    FIX: Remove plugin_data

--- disabled-cookies-warning.php
+++ disabled-cookies-warning.php
@@ -5,7 +5,7 @@
  * Description: A simple plugin to warn users if cookies are disabled in their browser.
  * Author: Ivo Jerković
  * Author URI: https://twitter.com/ijerkov
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -20,10 +20,10 @@ class Disabled_Cookies_Warning {
 
 	public function enqueue_scripts() {
 
-		$plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . plugin_basename( __FILE__ ) );
-		wp_register_script('disabled-cookies-warning-script', plugin_dir_url(__FILE__) . 'js/disabled-cookies-warning.js', array(), $plugin_data['Version'], true);
+		wp_register_script('disabled-cookies-warning-script', plugin_dir_url(__FILE__) . 'js/disabled-cookies-warning.js', array(), '1.0.1', true);
 		wp_enqueue_script('disabled-cookies-warning-script');
-		wp_enqueue_style('disabled-cookies-warning-style', plugin_dir_url(__FILE__) . 'css/disabled-cookies-warning.css', array(), $plugin_data['Version']);
+		wp_enqueue_style('disabled-cookies-warning-style', plugin_dir_url(__FILE__) . 'css/disabled-cookies-warning.css', array(), '1.0.1');
+
 
 		add_filter('script_loader_tag', function ($tag, $handle) {
 			if ('disabled-cookies-warning-script' !== $handle) {
